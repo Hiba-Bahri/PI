f132d031d201acca33a565a82ede6e5a7be67370
hiba bahri <hibabahri447@gmail.com>
2023-11-06 00:20:16 +0100

    Database sql file

--- database/cotek.sql
+++ database/cotek.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : Dim 05 nov. 2023 à 13:06
--- Version du serveur :  10.4.17-MariaDB
--- Version de PHP : 8.0.2
+-- Généré le : lun. 06 nov. 2023 à 00:10
+-- Version du serveur : 10.4.27-MariaDB
+-- Version de PHP : 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,29 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `equipe`
+-- Structure de la table `member`
 --
 
-CREATE TABLE `equipe` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `members` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `member` (
+  `disponibilite` varchar(15) NOT NULL,
+  `profession` varchar(50) NOT NULL,
+  `member_id` bigint(20) NOT NULL,
+  `team_id` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `projet`
+-- Structure de la table `team`
 --
 
-CREATE TABLE `projet` (
-  `id` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `owner` varchar(255) NOT NULL,
-  `keywords` varchar(255) NOT NULL,
-  `technologies` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `team` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -55,29 +52,28 @@ CREATE TABLE `projet` (
 --
 
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `first_name` varchar(25) NOT NULL,
-  `last_name` int(25) NOT NULL,
-  `email` int(255) NOT NULL,
-  `role` varchar(50) NOT NULL,
-  `profession` varchar(255) NOT NULL,
-  `disponibilite` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id` bigint(20) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Index pour les tables déchargées
 --
 
 --
--- Index pour la table `equipe`
+-- Index pour la table `member`
 --
-ALTER TABLE `equipe`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`member_id`),
+  ADD KEY `FKcjte2jn9pvo9ud2hyfgwcja0k` (`team_id`);
 
 --
--- Index pour la table `projet`
+-- Index pour la table `team`
 --
-ALTER TABLE `projet`
+ALTER TABLE `team`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -91,22 +87,27 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT pour la table `equipe`
+-- AUTO_INCREMENT pour la table `team`
 --
-ALTER TABLE `equipe`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `projet`
---
-ALTER TABLE `projet`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `team`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `member`
+--
+ALTER TABLE `member`
+  ADD CONSTRAINT `FKcjte2jn9pvo9ud2hyfgwcja0k` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`),
+  ADD CONSTRAINT `FKmj232ddq5f2n6g0kjt2h4jscd` FOREIGN KEY (`member_id`) REFERENCES `user` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
